53b8d959e2d91da1e529774e5c549976d1bbb8f7
valegusso <valeria.gusso@gmail.com>
2017-05-03 15:15:25 -0300

    added function to first exercise

--- coderhouse/first_exercise/sintaxis_variables.js
+++ coderhouse/first_exercise/sintaxis_variables.js
@@ -1,20 +1,35 @@
-//First exercise//
+/*  CoderHouse 03052017
+  	First exercise
+*/
 
-/*var nombre = prompt("Tell me your name");
-var texto = alert("Hi " + nombre + ", how are you today?");*/ 
+function firstExercise(){
+	var nombre = prompt("Tell me your name");
+	var texto = alert("Hi " + nombre + ", how are you today?");
+}
 
-//Second exercise//
 
-/*var number1 = prompt("Write a number without decimals");
+function secondExercise(){
+var number1 = prompt("Write a number without decimals");
 var number2 = 20
-var sum = alert(number2 + (+ number1));*/ 
+var sum = alert(number2 + (+ number1));
+}
 
-//Third exercise//
-
-/*var number1 = prompt("Write a number without decimals");
+function thirdExercise(){
+var number1 = prompt("Write a number without decimals");
 var number2 = 50
-var sum = alert(number1 - ( number2)); */
+var sum = alert(number1 - ( number2)); 
+}
 
-//Fourth exercise//
+function fourthExercise(){
+	var number1 = prompt("Write a number without decimals");
+	var number2 = prompt("Write other number without decimals");
+	//console.log(typeof(number1), typeof(number2));
+	var number1 = Number(number1);
+	var number2 = Number(number2);
+	var total = alert("The result is: " + (number1 + number2) );
+}
 
-//test
+//firstExercise();
+//secondExercise();
+thirdExercise();
+//fourthExercise();
