3023396659a168c0dc6e0f87dae52c193af9b233
valegusso <valeria.gusso@gmail.com>
2017-05-03 14:11:49 -0300

    exercise four done

---
+++ coderhouse/first_exercise/sintaxis_variables.js
@@ -0,0 +1,16 @@
+//First exercise//
+
+/*var nombre = prompt("Tell me your name");
+var texto = alert("Hi " + nombre + ", how are you today?");*/ 
+
+//Second exercise//
+
+/*var number1 = prompt("Write a number without decimals");
+var number2 = 20
+var sum = alert(number2 + (+ number1));*/ 
+
+//Third exercise//
+
+var number1 = prompt("Write a number without decimals");
+var number2 = 50
+var sum = alert(number1 - ( number2)); 
