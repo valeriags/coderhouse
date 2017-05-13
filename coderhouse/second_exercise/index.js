c0733e3e35ab2aa44c5f32eb67dfb1c0504de7c5
valegusso <valeria.gusso@gmail.com>
2017-05-13 19:06:14 -0300

    segundo ejercicio replanteado

--- coderhouse/second_exercise/index.js
+++ coderhouse/second_exercise/index.js
@@ -1,42 +1,40 @@
-//PRIMER EJERCICIO//
 
-/*var diasSemana = ["Lunes", "Martes","Miercoles","Jueves", "Viernes", "Sábado", "Domingo"];
-for(i=0; i<=6; i++){
+//EJERCICIO CONTROL DE FLUJOS//
+/*
+var diasSemana = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
 
-	if(diasSemana[i] === "Sábado") {
-		console.log("Hoy es: " + diasSemana[i] +":)");
+
+for(i in diasSemana){
+	if(diasSemana[i]=="miercoles"){
+		alert("Hoy es miercoles");
+	continue;
 	}
-	else if(diasSemana[i]==="Domingo") {	
-		console.log("Hoy es: " + diasSemana[i] + ":(");
+	else if(diasSemana[i]=="sabado"){
+		alert("Hoy es sabado");
+		break
 	}
-	else{
-		console.log("Hoy es: " + diasSemana[i]+ ", el fin de semana aún no llega");
+}
+*/
+
+//TRABAJO PRACTICO CONTROL DE FLUJOS//
+
+var diasSemana = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
+var diaCliente = prompt("Ingrese un dia de la semana");
+
+for(i in diasSemana){
+
+ 	if((i<=4)&&(diaCliente==diasSemana[i])){
+		console.log("Tenes una coincidencia con: " + diasSemana[i] + ". El día es laboral");
+		break;
+		}
+	else if(i>=5){
+		console.log("El dia ingresado es: " + diaCliente + ". El día es fin de semana");
+		break
 	}
-}*/
-
-//SEGUNDO EJERCICIO//
-
-var diasSemana = ["lunes","martes","miércoles", "jueves", "viernes", "sábado","domingo"];
-var diaCliente = prompt("Ingresa un día cualquiera").toUpperCase();
-
-
-for(i=0; i<diasSemana.length; i++) {
-
-	if(diaCliente == diasSemana[i])
-		console.log(diasSemana);
-	
 }
 
-	/*
-	if((diaCliente == diasSemana[i].toUpperCase()) && (diaCliente == [5] || diaCliente == [6])) {
-		console.log("el día es: ", diasSemana[i], "y No es laboral");
-		break;
-	}
-	
-	else if (diasSemana != diasSemana){
-		console.log("No hay coincidencia o el valor ingresado no es válido");
-	}
-	*/
-//console.log("Tenés una coincidencia con " + diasSemana[i] + ". El día es laboral");//
-	
-//&& (diaCliente !== [5] || diaCliente !== [6]))// {
+
+
+
+
+
