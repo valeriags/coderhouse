325561384e25e1a89f1d8dcf1c0beb30af5ad37c
valegusso <valeria.gusso@gmail.com>
2017-05-23 15:12:45 -0300

    Updated folrders

--- coderhouse/fifth_class/trabajo_practico/index.js
+++ coderhouse/fifth_class/trabajo_practico/index.js
@@ -37,7 +37,7 @@ class Pelicula {
 }
 
 //Creo la clase
-var Movie = new Pelicula("Volver al futuro");
+var Movie = new Pelicula();
 Movie.nombre = "Volver al futuro";
 Movie.id = "Ciencia ficcion";
 Movie.descripcion = "Marty McFly, un estudiante secundario de 17 es accidentalmente enviado 30 años al pasado en una maquina del tiempo DeLorean inventada por su mejor amigo el Doctor Brown.";
@@ -45,6 +45,11 @@ Movie.año = "1985";
 Movie.duracion = "1h 56min";
 Movie.actores = "Michael J. Fox, Christopher Lloyd, Lea Thompson";
 Movie.director = "Robert Zemeckis";
+
+//Creo un nuevo clase que comparta propuedades y metodos específicos
+
+ Movie_Dos.prototype = new Pelicula(),
+
 //Creo los métodos
 Movie.nombrePelicula();
 Movie.generoPelicula();
@@ -53,3 +58,10 @@ Movie.añoPelicula();
 Movie.duracionPelicula();
 Movie.actoresPelicula();
 Movie.directorPelicula();
+
+//Enunciado
+
+// Crear una funcion que agregue objetos peliculas
+//
+// El objeto pelicula Debera tener ID, titulo, descripcion, año, duracion, actores, director
+// Debera tener metodos para editar todas sus propiedades, menos su ID, y para los actores tendra que ir agregando uno a uno
