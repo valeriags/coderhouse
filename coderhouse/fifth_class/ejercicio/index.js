b2f20907e0048bfe1a4d23c970f233708e445334
valegusso <valeria.gusso@gmail.com>
2017-05-23 12:04:15 -0300

    Updated folders

--- coderhouse/fifth_class/ejercicio/index.js
+++ coderhouse/fifth_class/ejercicio/index.js
@@ -2,42 +2,53 @@
 //Creo mi constructor de objeto Auto y listo las distintas instancias de la propiedad que va a tener mi auto
 class Auto {
 
-    constructor(color, modelo, patente, marca, prendido, velocidad, velocidad_maxima){
-          this.color = color;
-          this.modelo = modelo;
-          this.patente = patente;
-          this.marca = marca;
-          this.prendido = prendido;
-          this.velocidad = velocidad;
-          this.velocidad_maxima = velocidad_maxima;
-        }
+  constructor(color, modelo, patente, marca, encendido, velocidad, velocidad_maxima) {
+    this.color = color;
+    this.modelo = modelo;
+    this.patente = patente;
+    this.marca = marca;
+    this.encendido = encendido;
+    this.velocidad = velocidad;
+    this.velocidad_maxima = velocidad_maxima;
+  }
 
-    // Defino los metodos de la clase
-    encender(){
-      if (this.prendido == true) {
-        // console.log("El auto esta prendido !");
-        return true;
-      } else if (this.prendido == false){
-        // console.log("El auto esta apagado !");
-        return false;
-      }
+  // Defino los metodos de la clase
+  encender() {
+    if (this.encendido == true) {
+      // console.log("El auto esta prendido !");
+      return true;
+    } else if (this.encendido == false) {
+      // console.log("El auto esta apagado !");
+      return false;
     }
-    acelerar(){
-      if (this.encender() == true){
-        console.log("El auto esta prendido !");
-        while (this.velocidad <= this.velocidad_maxima) {
-                        console.log("El auto ahora corre a la velocidad: " + this.velocidad);
-                        this.velocidad = this.velocidad + 25;
-                       }
-      }else {
-        console.log("Auto agapado")
+  }
+  acelerar() {
+    if (this.encender() == true) {
+      console.log("El auto esta encendido!");
+
+      while (this.velocidad <= this.velocidad_maxima) {
+        if (this.velocidad == 0) {
+          console.log("Ahora el auto esta acelerando");
+        } else
+          console.log("El auto ahora corre a una velocidad de: " + this.velocidad + " Km/h");
+        this.velocidad = this.velocidad + 25;
       }
     }
   }
+  apagar() {
+    if (this.velocidad != 0) {
+      console.log("El auto ha frenando y ahora puede apagarse");
+    }
+  }
+}
 
-var Smart = new Auto("Negro", "Smart Fortwo", "2013", "Smart", false, 0, 150 );
-// Smart.encender();
+
+//Defino la clase que dará valor a las propiedades del constructor
+var Smart = new Auto("Negro", "Smart Fortwo", "2013", "Smart", true, 0, 150);
+//Invoco a los métodos
+Smart.encender();
 Smart.acelerar();
+Smart.apagar();
 
 
 
